297d600cd623a4257799d116ea5dce4d6db93fc7
fuwutu <fuwutu@qq.com>
2013-07-03 22:04:13 +0800

    Create 50A - Domino piling.cpp

---
+++ 50A - Domino piling.cpp
@@ -0,0 +1,12 @@
+//4006895   Jul 3, 2013 6:00:13 PM	fuwutu	 50A - Domino piling	 GNU C++0x	Accepted	15 ms	0 KB
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int M, N;
+    cin >> M >> N;
+    cout << M * N / 2 << endl;
+    return 0;
+}
