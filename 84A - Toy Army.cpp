a1268ab4082151e3c697a42abf8eb222b76b3c27
fuwutu <fuwutu@qq.com>
2013-07-07 15:19:07 +0800

    Create 84A - Toy Army.cpp

---
+++ 84A - Toy Army.cpp
@@ -0,0 +1,12 @@
+//4033542   Jul 7, 2013 11:18:30 AM	fuwutu	 84A - Toy Army	 GNU C++0x	Accepted	15 ms	0 KB
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << n + n / 2 << endl;
+    return 0;
+}
