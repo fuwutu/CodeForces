36d6c9e2d8faedac964a33b51f5faf6d654ab0d6
fuwutu <fuwutu@qq.com>
2013-07-07 16:49:02 +0800

    Create 114A - Cifera.cpp

---
+++ 114A - Cifera.cpp
@@ -0,0 +1,24 @@
+//4033910   Jul 7, 2013 12:48:27 PM	fuwutu	 114A - Cifera	 GNU C++0x	Accepted	15 ms	0 KB
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int k, l, importance(-1);
+    cin >> k >> l;
+    while (l % k == 0)
+    {
+        l /= k;
+        importance += 1;
+    }
+    if (importance >= 0 && l == 1)
+    {
+        cout << "YES" << endl << importance << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
