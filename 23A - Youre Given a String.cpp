cc0b94af61b72e990b332df3f1f6eb15ad643d69
fuwutu <fuwutu@qq.com>
2013-08-13 09:02:54 +0800

    Create 23A - Youre Given a String.cpp

---
+++ 23A - Youre Given a String.cpp
@@ -0,0 +1,22 @@
+//4271120	 Aug 13, 2013 5:01:37 AM	fuwutu	 23A - Youre Given a String...	 Python 3	Accepted	 342 ms	 0 KB
+s = input()
+n = len(s)
+m = n - 1;
+while m > 0:
+    find = False
+    for i in range(0, n - m):
+        for j in range(i + 1, n - m + 1):
+            match = True
+            for k in range(0, m):
+                if s[i+k] != s[j+k]:
+                    match = False
+                    break
+            if match:
+                find = True
+                break
+        if find:
+            break
+    if find:
+        break
+    m -= 1
+print(m)
