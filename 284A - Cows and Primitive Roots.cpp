cad68f1883e7e50a48e1149dc1b320de70b80d24
fuwutu <fuwutu@qq.com>
2013-08-18 12:37:13 +0800

    Update 284A - Cows and Primitive Roots.cpp

--- 284A - Cows and Primitive Roots.cpp
+++ 284A - Cows and Primitive Roots.cpp
@@ -32,3 +32,32 @@ int main()
     printf("%d\n", count);
     return 0;
 }
+
+
+//4306655	 Aug 18, 2013 8:36:13 AM	fuwutu	 284A - Cows and Primitive Roots	 GNU C++0x	Accepted	30 ms	0 KB
+#include <cstdio>
+
+int main()
+{
+    int p;
+    scanf("%d", &p);
+    int n = p - 1;
+    int count = n;
+    for (int i = 2; i * i <= n; ++i)
+    {
+        if (n % i == 0)
+        {
+            do
+            {
+                n /= i;
+            } while (n % i == 0);
+            count = count / i * (i - 1);
+        }
+    }
+    if (n != 1)
+    {
+        count = count / n * (n - 1);
+    }
+    printf("%d\n", count);
+    return 0;
+}
